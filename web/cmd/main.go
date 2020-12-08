b4f66468f4db2346fc97e90d30ca5abf7b98614d
saiton03 <saiton15603@gmail.com>
2020-12-08 22:42:02 +0900

    improve UI for smart phone user

--- web/cmd/main.go
+++ web/cmd/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+<<<<<<< Updated upstream
 	"log"
 
+=======
+>>>>>>> Stashed changes
 	"github.com/gin-contrib/multitemplate"
 	"github.com/gin-gonic/gin"
 	"github.com/westlab/glory/web"
+	"log"
+	"os"
+	"time"
 )
 
 func createRender() multitemplate.Renderer {
@@ -17,8 +23,22 @@ func createRender() multitemplate.Renderer {
 }
 
 func main() {
+	Port := os.Getenv("GLORY_PORT")
+	if Port == "" {
+		Port = ":3000"
+	}
 	engine := gin.Default()
 	//engine.LoadHTMLGlob("templates/*.html")
+	if gin.Mode() == "release" {
+		logFile := "log/glory_log_" + time.Now().Format(time.RFC3339) + ".log"
+		f, err := os.Create(logFile)
+		if err != nil {
+			log.Fatalf("log file %s is invalid", logFile)
+		}
+		gin.DefaultErrorWriter = f
+		gin.DefaultWriter = f
+		engine.Use(gin.LoggerWithWriter(f))
+	}
 	engine.HTMLRender = createRender()
 	engine.Static("/public", "./public")
 	engine.StaticFile("/favicon.ico", "./public/favicon.ico")
@@ -27,7 +47,7 @@ func main() {
 	engine.GET("/data/:id", web.ProgressHandler)
 	engine.NoRoute(web.NotFoundHandler)
 
-	err := engine.Run(":3000")
+	err := engine.Run(Port)
 	if err != nil {
 		log.Fatal(err)
 	}
