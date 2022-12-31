a8cb299f05f04e08235f88dbe59d2e29efb7d829
Akihito Nishikawa <akihito.n.0910@keio.jp>
2022-12-31 17:40:17 +0900

    init 関数の移動とログ取得のための環境変数

--- golang/handler.go
+++ golang/handler.go
@@ -10,18 +10,8 @@ import (
 	"github.com/Songmu/flextime"
 	"github.com/gin-gonic/gin"
 
-	"github.com/westlab/glory/config"
 	"github.com/westlab/glory/db"
 )
-
-var gloryConfig *config.Conf
-
-func init() {
-	var err error
-	if gloryConfig, err = config.LoadConfig("./config.json"); err != nil {
-		log.Fatal(err)
-	}
-}
 
 func IndexHandler(ctx *gin.Context) {
 	ctx.HTML(http.StatusOK, "index", gin.H{
@@ -32,19 +22,25 @@ func IndexHandler(ctx *gin.Context) {
 }
 
 func ProgressHandler(ctx *gin.Context) {
+
 	id := ctx.Param("id")
-	for _, wg := range gloryConfig.WorkingGroups {
+	for _, wg := range GloryConfig.WorkingGroups {
 		if id == wg.Title {
+
 			th, err := FetchAllHistory(id)
 			if err != nil {
 				log.Printf("fetch history error: %v", err)
 			}
+
 			chart := NewProgressChart(wg, th)
+			//ranking := fetchRanking(wg)
+
 			ctx.HTML(http.StatusOK, "progress", gin.H{
 				"title":          wg.Describe,
 				"heading":        wg.Describe + "の進捗状況",
 				"working_groups": *getWorkingGroup(),
 				"chart":          chart,
+				//"ranking":        ranking,
 			})
 			return
 		}
@@ -53,14 +49,14 @@ func ProgressHandler(ctx *gin.Context) {
 }
 
 func NotFoundHandler(ctx *gin.Context) {
-	ctx.HTML(http.StatusOK, "notFound", gin.H{
+	ctx.HTML(http.StatusNotFound, "notFound", gin.H{
 		"working_groups": *getWorkingGroup(),
 	})
 }
 
 func getWorkingGroup() *[]string {
 	var ret []string
-	for _, wg := range gloryConfig.WorkingGroups {
+	for _, wg := range GloryConfig.WorkingGroups {
 		ret = append(ret, wg.Title)
 	}
 	return &ret
@@ -68,7 +64,7 @@ func getWorkingGroup() *[]string {
 
 func calcDeadlines() *map[string]string {
 	ret := map[string]string{}
-	for _, wg := range gloryConfig.WorkingGroups {
+	for _, wg := range GloryConfig.WorkingGroups {
 		var left string
 		deadline, err := time.Parse(db.DateFormat, wg.Deadline)
 		if err != nil {
@@ -85,13 +81,17 @@ func calcDeadlines() *map[string]string {
 }
 
 func FetchAllHistory(wg string) ([]*ThesisHistoryJoinAuthor, error) {
+
 	var ret []*ThesisHistoryJoinAuthor
 
-	rows, _ := db.DB.Query("SELECT name, char_count, fetch_time FROM thesis_history JOIN author ON author.author_id=thesis_history.author_id WHERE working_group=? and fetch_time <= ? ORDER BY fetch_time", wg, flextime.Now().UTC().Format(db.TimeFormat))
+	query := `SELECT name, char_count, fetch_time
+		FROM thesis_history JOIN author ON author.author_id=thesis_history.author_id
+		WHERE working_group=? and fetch_time <= ? ORDER BY fetch_time`
+	rows, _ := db.DB.Query(query, wg, flextime.Now().UTC().Format(db.TimeFormat))
+	defer rows.Close()
 	if db.IsNoRows(rows.Err()) {
 		return nil, nil
 	}
-	defer rows.Close()
 
 	for rows.Next() {
 		var th ThesisHistoryJoinAuthor
@@ -100,10 +100,12 @@ func FetchAllHistory(wg string) ([]*ThesisHistoryJoinAuthor, error) {
 		if err != nil {
 			return nil, fmt.Errorf("fetch record error: %w", err)
 		}
+
 		th.FetchTime, err = time.Parse(db.TimeFormat, ft)
 		if err != nil {
 			return nil, fmt.Errorf("convert to time error: %w", err)
 		}
+
 		th.FetchTime = th.FetchTime.Local()
 		ret = append(ret, &th)
 	}
